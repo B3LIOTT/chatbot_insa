c4a76d0e2fcf17c3c5c28ef4972ac2885538bb7a
B3LIOTT <55959873+B3LIOTT@users.noreply.github.com>
2024-06-14 17:48:56 +0200

    Update env_loader.dart

--- lib/local/config/env_loader.dart
+++ lib/local/config/env_loader.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+// Variables d'environnement de l'app (en partie stockées dans .env pour les infos sensibles)
 
 class EnvLoader {
   static late String serverUrl;
